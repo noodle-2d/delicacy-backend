4fbb7691bf2337ffeb1f9887d62445bd816d490c
Ilya Kuznetsov <et.anchoret@gmail.com>
2017-09-24 11:24:59 +0300

    OrikaMapperFacadeFactory was corrected.

--- Delicacy/src/main/java/com/delicacycomics/delicacy/component/OrikaMapperFacadeFactory.java
+++ Delicacy/src/main/java/com/delicacycomics/delicacy/component/OrikaMapperFacadeFactory.java
@@ -3,15 +3,27 @@ package com.delicacycomics.delicacy.component;
 import ma.glasnost.orika.MapperFacade;
 import ma.glasnost.orika.MapperFactory;
 import ma.glasnost.orika.impl.DefaultMapperFactory;
+import org.springframework.beans.factory.FactoryBean;
 import org.springframework.stereotype.Component;
 
 @Component
-public class OrikaMapperFacadeFactory {
+public class OrikaMapperFacadeFactory implements FactoryBean<MapperFacade> {
 
-    public MapperFacade getFacade() {
+    @Override
+    public MapperFacade getObject() throws Exception {
         MapperFactory mapperFactory = new DefaultMapperFactory.Builder().build();
         createMappings(mapperFactory);
         return mapperFactory.getMapperFacade();
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return MapperFacade.class;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return true;
     }
 
     private void createMappings(MapperFactory mapperFactory) {
